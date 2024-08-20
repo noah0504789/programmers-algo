860f8e5ccdc84d142dbb392c845f7e75b2906218
김노아 <noah0504@naver.com>
2024-08-20 11:29:52 +0900

    가장 큰 수

--- src/main/java/org/example/sort/가장_큰_수.java
+++ src/main/java/org/example/sort/가장_큰_수.java
@@ -1,40 +1,32 @@
 package org.example.sort;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class 가장_큰_수 {
-    public static String solution(int[] numbers) {
-        // (맨 앞자리 수가 같음 -> 작은 수의 맨 앞자리와 큰 수의 맨 뒷자리 수를 비교 -> 큰 수가 앞으로)
-        // 맨 앞자리가 다름: 내림차순으로
-        // 맨 앞자리가 같음
-        // - 자릿수가 다른 경우: 뒷숫자로 판단해야 함
-
-        // 3, 30, 34, 5, 9
-
-        // 9 5
-        // 3, 30, 34 -> 34 3 30
-
-        // 34 30 3: 34303
-        // 34 3 30: 34330
-
-        // 3 30 34: 33034
-        // 3 34 30: 33430
-
-        // 30 34 3: 30343
-        // 30 3 34: 30334
-
-        // 9 5 34 3 30
-
-        // 3 30 34 => 한번씩 써서 가장 큰 수 만들기
-        // 34 3 30
-        //
-
-        String answer = "";
-
-        return answer;
-    }
 
     public static void main(String[] args) {
         System.out.println(solution(new int[]{6, 10, 2}));
         System.out.println(solution(new int[]{3, 30, 34, 5, 9}));
+    }
+
+    public static String solution(int[] numbers) {
+        if (numbers.length == 0) return "";
+
+        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+
+        Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
+
+        String ans = String.join("", arr);
+
+        int startIdx = 0, endIdx = ans.length();
+        while (ans.length() > 1 && ans.startsWith("0")) {
+            if (endIdx < 2) break;
+
+            endIdx--;
+        }
+
+        return ans.substring(startIdx, endIdx);
     }
 }
 
