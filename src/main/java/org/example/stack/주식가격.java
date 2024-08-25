b460be6d1ef71b9f4b93438b17f67cf0caa9fab7
noah0504789 <noah0504@naver.com>
2024-08-25 19:34:50 +0900

    stack: 주식가격

--- src/main/java/org/example/stack/주식가격.java
+++ src/main/java/org/example/stack/주식가격.java
@@ -1,8 +1,11 @@
 package org.example.stack;
 
+import java.util.Stack;
+
 public class 주식가격 {
     public static void main(String[] args) {
-        int[] solution = solution(new int[]{1, 2, 3, 2, 3});
+//        int[] solution = solution(new int[]{1, 2, 3, 2, 3});
+        int[] solution = solution(new int[]{1, 2, 3, 2, 3, 3, 1});
 
         for (int i = 0; i < solution.length; i++) {
             System.out.print(solution[i] + " ");
@@ -10,31 +13,8 @@ public class 주식가격 {
     }
 
     // prices
+    // - 가격이 떨어지지 않은 기간 (떨어지면 비교 X)
     // - n초간의 주가를 기록 (초 단위)
-
-    // 가격이 떨어지지 않은 기간 (떨어지면 비교 X)
-    public static int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-        for (int pointInTime = 0; pointInTime < prices.length-1; pointInTime++) {
-            // TODO: 부분집합 (시점 - 비교)
-            int period = 0;
-            for (int start = pointInTime+1; start < prices.length; start++) {
-                if (prices[pointInTime] > prices[start]) break;
-
-                period++;
-            }
-
-            if (period == 0) period = 1;
-
-            // TODO: 시점별 떨어지지 않은 시간 값 할당
-            answer[pointInTime] = period;
-        }
-
-        answer[prices.length-1] = 0;
-
-        return answer;
-    }
 
     // 첫번쨰 주식 관점 <1> (0) (1, 2, 3, 4)
     // prices 2 3 2 3
@@ -62,5 +42,43 @@ public class 주식가격 {
     //  3(4s) X
     // => 1
 
-    // 마지막 주식 => 무조건 0
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length-1 - idx;
+        }
+
+        return answer;
+    }
+
+    public static int[] solution2(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int pointInTime = 0; pointInTime < prices.length-1; pointInTime++) {
+            int period = 0;
+            for (int start = pointInTime+1; start < prices.length; start++) {
+                period++;
+
+                if (prices[pointInTime] > prices[start]) break;
+            }
+
+            answer[pointInTime] = period;
+        }
+
+        answer[prices.length-1] = 0;
+
+        return answer;
+    }
 }
